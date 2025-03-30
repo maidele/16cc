80e177b2fe558a6428b1efc9d7f46707ed2869e8
Maidele Archelus <maidele@usf.edu>
2025-03-30 15:02:53 -0400

    Task 2-Implement fetchProductsThen using .then() and .catch()

---
+++ cc_16.js
@@ -0,0 +1,24 @@
+//Task 2: Fetch Products with .then()
+
+const BASE_URL = "https://www.course-api.com/javascript-store-products";// URL to get data from
+function fetchProductsThen() {
+    fetch(BASE_URL)
+    .then (response => {
+        if (!response.ok) {
+            throw new Error(`Error regarding HTTP `)
+
+        }
+        return response.json();//
+    })// function is here to get data from the product
+
+    .then (products =>{
+        products.forEach(product =>{
+            console.log(product.fields.name);
+        });// this is where the name of the product is logged to the console
+    })
+
+    .catch(error => {
+        console.error ('Failure issue regarding fetch :', error);// catch is used to log errors
+    })
+
+}
